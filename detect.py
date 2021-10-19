a03a6b325d142e4f636fa1492c2ea65abd7f6f9c
Danilo Ferreira de Lima <danilo.enoque.ferreira.de.lima@xfel.de>
2021-10-19 09:07:06 +0200

    Added filter.

--- detect.py
+++ detect.py
@@ -2,7 +2,8 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.signal import welch
+from scipy.signal import welch, lfilter, butter, freqz
+from scipy.signal import find_peaks_cwt
 from matplotlib.animation import FuncAnimation
 import sounddevice as sd
 
@@ -14,28 +15,33 @@ def fetch_audio(fs:float,
 
 def live_plotter(i: int) -> None:
     n_octaves = 4
-    time = 0.1
+    time = 1.0
     fs = 44.1e3
     Ts = 1.0 / fs
     N = int(time / Ts)
+
+    f_s = 220.0
+    f_e = f_s*n_octaves
 
     x = np.linspace(0.0, N*Ts, N)
     y = fetch_audio(fs, N)
     y = np.mean(y, axis=-1)
 
+    b, a = butter(4, (f_s, f_e), btype='bandpass', output='ba', analog=False, fs=fs)
+    y = lfilter(b, a, y)
+
     F, Y = welch(y,
                  fs=fs,
-                 window="hann",
-                 nperseg=4096,
-                 noverlap=2048,
-                 scaling='spectrum')
+                 window="hamming",
+                 nperseg=N,
+                 )
+                 #scaling='spectrum')
     # Y = 1/T |FT(y)[x]|**2 in intensity^2 / Hz
-    Y = 10.0*np.log10(Y) # in dB
-    print(Y)
+    Y = 10*np.log10(Y) # in dB
 
-    # filter it
-    #window = np.hamming(20)
-    #y = np.convolve(y, window)
+    ## filter it
+    #window = np.hanning(N)
+    #y = y*window
 
     #Y = np.fft.fft(y)
     #Y = Y[:N//2]
@@ -46,8 +52,6 @@ def live_plotter(i: int) -> None:
     #F = F[:N//2]
     ##F = np.linspace(0.0, 0.5*fs, N//2)
 
-    f_s = 220.0
-    f_e = f_s*n_octaves
     i_s = np.where(F >= f_s)[0][0]
     i_e = np.where(F >= f_e)[0][0]
     select = slice(i_s, i_e)
@@ -60,11 +64,30 @@ def live_plotter(i: int) -> None:
     ax.set(xlabel='Frequency [Hz]',
            ylabel='Relative power [dB]',
            title='',
-           ylim=(-120.0, 0.0),
+           ylim=(-140.0, 0.0),
            xlim=(f_s, f_e))
+    idx = find_peaks_cwt(Y, np.arange(1, 10))
+    if len(idx) > 0:
+        print(f"Peaks found in frequencies: {F[idx]}")
 
 def main() -> None:
     #plt.style.use('fivethirtyeight')
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = plt.gca()
+    f_s = 220.0
+    f_e = 220.0*4
+    fs = 44.1e3
+    b, a = butter(4, (f_s, f_e), btype='bandpass', output='ba', analog=False, fs=fs)
+    w, h = freqz(b, a, fs=fs)
+    ax.semilogx(w, 20 * np.log10(abs(h)))
+    ax.set(title='Filter response', xlabel='Frequency [Hz]', ylabel='Amplitude [dB]', ylim=(-40, 0), xlim=(f_s/4, 4*f_e))
+    plt.margins(0, 0.1)
+    plt.grid(which='both', axis='both')
+    plt.axvline(f_s, color='green')
+    plt.axvline(f_e, color='green')
+    plt.show()
+    plt.close(fig)
 
     fig = plt.figure(figsize=(10, 8))
     ani = FuncAnimation(fig, live_plotter, interval=100)
